f691478faaab4c5621c3d2c7f51d497899f83af8
najibhrd25 <bahrudinmohammadnajib@gmail.com>
2025-10-25 16:01:46 +0700

    Fix HTTPS middleware order for Railway

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,10 +14,10 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // Middleware global (jalan di semua request)
-        \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\ForceHttps::class, // tambahkan ini
+    // Middleware global (jalan di semua request)
         \Illuminate\Http\Middleware\HandleCors::class,
+        \App\Http\Middleware\TrustProxies::class, // <- Pindahkan ke sini, sebelum ForceHttps
+        \App\Http\Middleware\ForceHttps::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
